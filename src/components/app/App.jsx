3369b1c67e8a58b49a686433b217cb411be99a31
Evelina <evelinaavegas@gmail.com>
2026-07-17 15:05:41 +0300

    components create

---
+++ src/components/app/App.jsx
@@ -0,0 +1,15 @@
+import { Routes, Route } from "react-router-dom";
+import Layout from '../layout/Layout'
+
+function App() {
+    return (
+        <Routes>
+            <Route path='/' element={<Layout />} >
+                
+            </Route>
+            
+        </Routes>
+    )
+}
+
+export default App
