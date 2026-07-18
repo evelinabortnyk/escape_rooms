0e24fb39c4358ff68c5287bb517c31ddff03bc19
Evelina <evelinaavegas@gmail.com>
2026-07-18 18:23:42 +0300

    for quest page create, add route

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,13 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Layout from '../layout/Layout'
 import Escapes from "../pages/home-page/Escapes";
+import Quest from "../pages/quest-page/Quest";
 
 function App() {
     return (
         <Routes>
             <Route path='/' element={<Layout />} >
                 <Route index element={<Escapes />} />
-                {/* <Route path="converter" element={<Converter />} /> */}
+                <Route path="/:title/:id" element={<Quest />} />
             </Route>
             
         </Routes>
