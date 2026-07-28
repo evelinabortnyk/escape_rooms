5d9c7799e1adec679edcdf8ffee3bbb0f667d403
Evelina <evelinaavegas@gmail.com>
2026-07-28 19:39:06 +0300

    error page create

---
+++ src/components/pages/not-found/NotFound.jsx
@@ -0,0 +1,16 @@
+import './not-found.css'
+
+function NotFound () {
+
+    return (
+        <div className='not-found--wrap'>
+            <div>
+                <p className='not-found--title'>Page not found</p>
+                <h3>Error 404</h3>
+            </div>
+            
+        </div>
+    )
+}
+
+export default NotFound
